f601315cf4d2a086cbe5155fb5666c7e422dff04
ahmed samir <ahmed_samir@khales.com.eg>
2023-09-22 17:20:26 +0300

    Added Service List and Maps Fragment - adding nav graph - adding gradle dependencies for -> (nav graph) - adding Service List Fragment - adding Maps Fragment

---
+++ app/src/main/java/com/samir/features/ui/activity/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.samir.features.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.samir.features.R
+import com.samir.features.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    var binding: ActivityMainBinding? = null
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+
+        setContentView(binding?.getRoot())
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.main_container) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.mapsFragment -> showBottomNav()
+                R.id.serviceListFragment -> showBottomNav()
+                else -> hideBottomNav()
+            }
+        }
+    }
+
+    private fun showBottomNav() {
+        binding?.bottomNavigationView?.visibility = View.VISIBLE
+
+    }
+
+    private fun hideBottomNav() {
+        binding?.bottomNavigationView?.visibility = View.GONE
+    }
+
+}
